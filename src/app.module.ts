03f83fe398f52d7a4b770c898d93a789ccf09a76
Hung Tran <hung.tran@smartdev.com>
2022-05-13 16:28:39 +0700

    update dynamic module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
-import { Module, ValidationPipe } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
-import { PrismaModule } from './prisma/prisma.module';
-import { APP_PIPE } from '@nestjs/core';
+import { Module } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import * as path from 'path';
+import { AppService } from './app.service';
+import { ModuleLoaderModule } from './common/module-loader.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  imports: [
+    EventEmitterModule.forRoot({ wildcard: true }),
+    /**
+     * Load all entity unit modules in subdirectory /db/entity
+     */
+    ModuleLoaderModule.register({
+      name: 'db-entities',
+      /**
+       * Make sure the path resolves to the **DIST** subdirectory, (we are no longer in TS land but JS land!)
+       */
+      path: path.resolve(__dirname, './db/entity'),
+      fileSpec: '**/*.module.js',
+    }),
+  ],
+  providers: [AppService]
 })
 export class AppModule { }
